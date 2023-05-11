5fb1ddbcb58656ff2cf7b157f2549d2b609d7bb6
kerzers <ahra.ahlame@gmail.com>
2023-05-11 14:20:14 -0700

    prompt command

---
+++ testing/prompt.c
@@ -0,0 +1,50 @@
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+/**
+ * main - reads line and prints it in terminal
+ *
+ *Return: 0 (Sucess)
+ */
+int main(void)
+{
+        ssize_t n;
+        size_t size = 0;
+        char *buffer = NULL;
+	pid_t cpid;
+	char *argv[2];
+	int status;
+
+	while (1)
+	{
+	printf("#cisfun$ ");
+        n = getline(&buffer, &size, stdin);
+        if (n == -1)
+                break;
+	argv[0] =strtok(buffer, "\n");
+	argv[1] = NULL;
+	cpid = fork();
+	if (cpid == -1)
+	{
+		perror("Error");
+		break;
+	}else if (cpid == 0)
+	{
+		if (execve(argv[0], argv, NULL) == -1)
+    		{
+        	perror("Error:");
+		break;
+ 		}
+	}
+	else
+		wait(&status);
+	}
+        free(buffer);
+        return (0);
+}
