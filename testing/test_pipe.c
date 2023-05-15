1d9bf7e5a0fae1e96fdced72d2cb52ef45211ae7
kerzers <ahra.ahlame@gmail.com>
2023-05-15 07:34:54 -0700

    test pipe

--- testing/test_pipe.c
+++ testing/test_pipe.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 
 int main(int ac, char **argv)
 {
-	(void)ac;
+	int fd;
+	char *buffer;
+	ssize_t n;
 
+	(void)ac;
 	if (isatty(0))
 	{
 		printf("... %s %s\n", argv[0], argv[1]);
 	}
 	else
 	{
-		printf("%s %s\n", argv[0], argv[1]);
+		fd = open(stdin, O_RDONLY);
+		if (fd == -1)
+		{
+			perror(argv[0]);
+		}
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+		return (0);
+	read(fd, buffer, 1024);
+
+		printf("%s %s\n", argv[0], buffer);
 	}
 	return (0);
 }
