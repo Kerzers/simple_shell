e1fd53af74342d0c375925efd6143dedb345b8ee
kerzers <ahra.ahlame@gmail.com>
2023-05-16 06:21:22 -0700

    path list

--- testing/4.linked_ls.c
+++ testing/4.linked_ls.c
@@ -7,18 +7,17 @@ dir_t *add_node_end(dir_t **head, const char *str);
  *@path: the value of PATH
  *Return: linked list of the PATH directories.
  */
-dir_t path_dir_ls(const char *path)
+dir_t *path_dir_ls(const char *path)
 {
-	dir_t **head;
+	dir_t *head = NULL;
 	const char *token;
 
 	
        	token = strtok((char *)path, ":");
         while (token)
         {
-        	add_node_end(head, token);
-                i++;
+        	add_node_end(&head, token);
                 token = strtok(NULL, ":");
         }
-	return (*head)
+	return (head);
 }
